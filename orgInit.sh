931c919921ffdaca2e050ac70f5763c3bc5f1f00
Jonathan Zhou <jonathan.zhou@jonathan-ltmcf4y.internal.salesforce.com>
2025-09-15 11:04:22 -0700

    remove package

--- orgInit.sh
+++ orgInit.sh
@@ -1,6 +1,7 @@
 sf demoutil org create scratch -f config/project-scratch-def.json -d 5 -s -p data -e detect.demo
-sf package install -w 20 -r -p 04t5e000000aVeP
+# sf package install -w 20 -r -p 04t5e000000aVeP
 sf project deploy start
+sf org assign permset -n Data_Detect_User
 sf demoutil user password set -p salesforce1 -g User -l User
 sf automig load --inputdir ./data
-sf org open -p /lightning/n/datadetect__Home
+sf org open -p /lightning/n/standard-DataDetect
