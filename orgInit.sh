83ec235c1ed46660dba92deff60f5050016b4788
Jonathan Zhou <jonathan.zhou@jonathan-ltmcf4y.internal.salesforce.com>
2025-11-18 17:01:30 -0800

    create policy

--- orgInit.sh
+++ orgInit.sh
@@ -3,5 +3,6 @@ sf demoutil org create scratch -f config/project-scratch-def.json -d 5 -s -p dat
 sf project deploy start
 sf org assign permset -n Data_Detect_User
 sf demoutil user password set -p salesforce1 -g User -l User
+sf apex run -f scripts/apex/dataDetectPolicyCreation.apex
 sf automig load --inputdir ./data
 sf org open -p /lightning/n/standard-DataDetect
